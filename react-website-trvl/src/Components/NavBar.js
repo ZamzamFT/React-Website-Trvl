30dc70b56316926d06045f3681cf02821c70f09d
ZamzamFT <ZamzamFahiyee@outlook.com>
2022-11-09 13:15:25 +0100

    first commit

---
+++ react-website-trvl/src/Components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <>
+     <nav className="navbar">
+        <div className="navbar-container">
+            <link to="/" className="navbar-logo" />
+            <h1>Hello World</h1>
+        </div>
+     </nav>
+    
+    </>
+  )
+}
+
+export default NavBar
